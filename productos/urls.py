f27458ee9ac66421daace318ef0f1d76a8eb63e6
Karen Sofia Lopez Reyes <lopezreyeskarensofia@gmail.com>
2026-09-22 10:40:09 -0500

    Eliminar __pycache__ y actualizar views/urls

--- productos/urls.py
+++ productos/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.lista_productos, name='lista_productos'),
+    path('lista_productos/', views.lista_productos, name='lista_productos'),
     path('crear/', views.crear_producto, name='crear_producto'),
     path('editar/<int:id>/', views.editar_producto, name='editar_producto'),
     path('eliminar/<int:id>/', views.eliminar_producto, name='eliminar_producto'),
