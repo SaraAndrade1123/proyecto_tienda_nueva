9019e12c786e7a7dd3d054906d70a92e67594250
Karen Sofia Lopez Reyes <lopezreyeskarensofia@gmail.com>
2026-09-22 09:40:19 -0500

    inicio_sesion

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from django.contrib.auth import views as auth_views
-
 from django.shortcuts import redirect
 
 
@@ -12,12 +9,13 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    #path('', lambda request: redirect('login')),
+    path('', lambda request: redirect('login')),
 
     path('', include('accounts.urls')),
 
     path('', include('gestor_inventario.urls')),
 
+    path('productos/', include('productos.urls')),
 ]
 
 if settings.DEBUG:
